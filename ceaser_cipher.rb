89d868fa4c72234b7b84d566afbe11ad77963d99
Ella Jozinovic <ellajozi01@gmail.com>
2024-12-07 13:19:11 +1030

    Fleshed out logic, method is working

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -1,12 +1,20 @@
-# What I want:
-# > caesar_cipher("What a string!", 5)
-# => "Bmfy f xywnsl!"
-
 def caesar_cipher(string, shift)
   ascii_arr = string.split("").map { |char| char.ord }
-  p ascii_arr
-  shifted_string = ascii_arr.map { |char| char.to_i - shift }.map { |char| char.chr }
-  p shifted_string
+  transformed = ascii_arr.map do |char| 
+    if char >= 65 && char <= 90
+      # Character is uppercase
+      ((char - 65 + shift) % 26 + 65).chr # Shift logic
+    elsif char >= 97 && char <= 122
+      # Character is lowercase
+      ((char - 97 + shift) % 26 + 97).chr
+    else
+      char.chr
+    end
+  end
+
+  # shifted_string = ascii_arr.map { |char| char.to_i - shift }.map { |char| char.chr }
+  
+  transformed.join("")
 end
 
-caesar_cipher("abcdefg", 4)
+puts caesar_cipher("What a string!", 5)
