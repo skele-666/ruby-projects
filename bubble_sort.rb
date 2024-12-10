c316195269ac283b678678c855d4f12b5a34b703
Ella Jozinovic <ellajozi01@gmail.com>
2024-12-10 12:03:41 +1030

    Removed large comment

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,35 +1,3 @@
-# (n - 1) times where n is the number of elements in the array (arr.length - 1):
-# FOR EACH elt IN list
-# look at elt and elt to the immediate right
-#   IF out of order
-#     SWAP elements
-#   END
-# END
-# 
-# After ONE pass through the entire list, it is guaranteed that the greatest element will be in the correct place.
-# Therefore, in subsequent passes, we do not need to check the last element(s), only needing to check the remaining unsorted
-# After k iterations, checking the last k elements is redundant.
-#
-# [3, 2, 1]
-# [2, 3, 1] => [2, 1, 3]
-# [1, 2, 3]
-
-# (n - 1) times where n is the number of elements in the array (arr.length - 1):
-# FOR EACH elt IN list
-# look at elt and elt to the immediate right
-#   IF out of order
-#     SWAP elements
-#   END
-# END
-# 
-# After ONE pass through the entire list, it is guaranteed that the greatest element will be in the correct place.
-# Therefore, in subsequent passes, we do not need to check the last element(s), only needing to check the remaining unsorted
-# After k iterations, checking the last k elements is redundant.
-#
-# [3, 2, 1]
-# [2, 3, 1] => [2, 1, 3]
-# [1, 2, 3]
-
 def bubble_sort(arr)
   n = arr.length
   comparison_count = 0
